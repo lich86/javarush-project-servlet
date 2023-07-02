b6445f12835c516995cef01f8df7037c4ddda159
Elizaveta Chervonnaya <ntropy@bk.ru>
2023-07-03 02:32:33 +0300

    add InitServlet tests and RestartServlet tests

--- src/test/java/com/tictactoe/RestartServletTest.java
+++ src/test/java/com/tictactoe/RestartServletTest.java
@@ -1,6 +1,5 @@
 package com.tictactoe;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
